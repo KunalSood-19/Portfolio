10df32995e9d94edcfbfa4655ea9041801b826f9
Kunal Sood <kunaalood15@gmail.com>
2025-03-12 23:39:18 +0530

    changes commited

--- projects.js
+++ projects.js
@@ -3,27 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger");
     const navLinks = document.querySelector(".nav-links");
 
-    // Load Dark Mode Preference
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark-mode");
-        themeToggle.textContent = "☀️";
-    } else {
-        themeToggle.textContent = "🌙";
-    }
-
-    // Toggle Dark Mode
-    themeToggle.addEventListener("click", () => {
-        document.body.classList.toggle("dark-mode");
-
-        if (document.body.classList.contains("dark-mode")) {
-            localStorage.setItem("theme", "dark");
-            themeToggle.textContent = "☀️";
-        } else {
-            localStorage.setItem("theme", "light");
-            themeToggle.textContent = "🌙";
-        }
-    });
-
+ 
     // Mobile Menu Toggle
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("active");
